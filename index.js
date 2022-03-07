1a3b15f19471d470fc2780f8efc1b5703bfcd7f6
Jason Mullings <wjasonmullings@gmail.com>
2022-03-07 15:31:31 -0500

    added multi tag functionality and removed doubles

--- index.js
+++ index.js
@@ -11,23 +11,65 @@ console.log("Server running on port 3000");
 const testPing = (req, res) => {
   res.send({
     "success": true
-  })
+  });
 }
 //route 2
 //this function makes the 'get' call using axios
 //using the guide from https://expressjs.com/en/guide/routing.html I make my route with the app.get method
 const getPostsByTags = (req, res) => {
-  console.log(req.params.tags)
-      axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${req.params.tags}`)
+
+  //list of tags submitted by user
+  let tags = req.params.tags.split(',');
+  let promises = [];
+  //if user submits multiple tags save them to this variable
+  let multipleResponses = {posts:[]};
+ 
+  // check amount of tags 
+  //if only one tag push resp to multipleResponses array
+  if (tags.length === 1) {
+    promises.push(
+      axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${tags}`)
         .then(resp => {
-          res.send(resp.data.posts)
-      })
-      .catch(err => {
-        // Handle Error Here
-        res.send(`error: ${err.response.data.error}`);
-      });
+          resp.data.posts.map((element) => multipleResponses.posts.push(element))
+        })
+        .catch(err => {
+          // Handle Error Here
+          console.log(err);
+        }));
+      } else {
+    for (let i = 0; i < tags.length; i++){
+      //push the response from the first tag to multipleResponses
+      if (i === 0) {
+        promises.push(
+          axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${tags[i]}`)
+            .then(resp => {
+              resp.data.posts.map((element)=> multipleResponses.posts.push(element))
+            })
+            .catch(err => {
+              // Handle Error Here
+              console.log(err)
+            }))
+      } else {
+        //get the data from the api
+        //loop through the data and push element to multipleResponses if element is not in multipleResponses already
+        promises.push(
+          axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${tags[i]}`)
+            .then(resp => {
+              resp.data.posts.map((element) => { multipleResponses.posts.includes(element) ? null : multipleResponses.posts.push(element) });
+            })
+            .catch(err => {
+              // Handle Error Here
+              console.log(err);
+            }))
+      };
+    };
+  };
+
+  Promise.all(promises).then(() => res.status(400).send(multipleResponses));
 };
 
 app.get('/api/ping', testPing);
 
 app.get('/api/:tags', getPostsByTags);
+//sends error message if no tags are presented in url
+app.get('/api/', (req, res) => res.send({ "error": "Tags parameter is required" }));
